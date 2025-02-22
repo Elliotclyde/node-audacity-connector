6f4a9e5c9029fed06d28653c16b7f9c2353d490a
Hugh Haworth <hughelliotclyde@gmail.com>
2025-02-22 16:46:13 +1300

    Remove the specific plugin I use

--- examplescript.mjs
+++ examplescript.mjs
@@ -6,7 +6,6 @@ await connector.openAudacity();
 
 await connector.sendCommandToAudacity(`Import2: FileName=${import.meta.dirname}\\${process.argv[2]}`)
 await connector.sendCommandToAudacity("Select: Track=0")
-await connector.sendCommandToAudacity("RnnoiseSuppressionForVoice: Use_Preset=\"Chill\"")
 await connector.sendCommandToAudacity("TruncateSilence: Action=\"Truncate Detected Silence\" Compress=\"50\" Independent=\"0\" Minimum=\"1.0\" Threshold=\"-40\" Truncate=\"0.25\"")
 await connector.sendCommandToAudacity("ClickRemoval:Threshold=\"200\" Width=\"20\"")
 await connector.sendCommandToAudacity("LoudnessNormalization:DualMono=\"1\" LUFSLevel=\"-20\" NormalizeTo=\"0\" RMSLevel=\"-20\" StereoIndependent=\"0\"")
